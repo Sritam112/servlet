07fb70463394c2fd396570c1f253d5af02496e6e
Sritam <Sritam@Sritam>
2021-02-16 17:56:17 +0530

    UPDATE

--- VOTER/src/servlet/nt/com/Vote.java
+++ VOTER/src/servlet/nt/com/Vote.java
@@ -6,18 +6,19 @@ import java.net.http.HttpResponse;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-public class Vote {
+public class Vote extends HttpServlet
+{
 public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException, IOException{
 	PrintWriter pw=res.getWriter();
 	res.setContentType("text/html");
 	String name=req.getParameter("pname");
 	int page=Integer.parseInt(req.getParameter("page"));
 		String addr=req.getParameter("addr");
-		if (page<18) 
+		if (page<18)
 			pw.println("<h1 style='color:red;text-align='center'>Mr/Miss/Mrs."+name+" u r not elgible to vote</h1>" );
 			else
 			pw.println("<h1 style='color:green;text-align='center'>Mr/Miss/Mrs."+name+" u r  elgible for voting</h1>" );
- pw.println("<a href='http://localhost:4040/VOTER/form.html'></a>");
+ pw.println("<a href='http://localhost:4040/VOTER/form.html'><img src='image/home.png'></a>");
 		pw.close();
 }
 	
